3887bafbaef44cbedfabd542b28deb5240441ddb
phananh2005 <phantran14122005@gmail.com>
2026-05-26 10:21:11 +0700

    base finish

---
+++ src/main/java/com/phananh/e_commerce/usermanagement/presentation/controller/CustomerUserController.java
@@ -0,0 +1,55 @@
+package com.phananh.e_commerce.usermanagement.presentation.controller;
+
+import com.phananh.e_commerce.core.presentation.dto.response.ApiResponse;
+import com.phananh.e_commerce.usermanagement.application.dto.response.UserInfoResponse;
+import com.phananh.e_commerce.usermanagement.application.service.UserService;
+import com.phananh.e_commerce.usermanagement.presentation.dto.request.UserChangePasswordRequest;
+import com.phananh.e_commerce.usermanagement.presentation.dto.request.UserInfoUpdateRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Tag(name = "Người dùng", description = "Các API quản lý thông tin tài khoản người dùng")
+public class CustomerUserController {
+
+    UserService userService;
+
+    //customer
+    @GetMapping("/my-info")
+    @Operation(summary = "Lấy thông tin cá nhân")
+    public ResponseEntity<ApiResponse<UserInfoResponse>> getMyInfo() {
+        ApiResponse<UserInfoResponse> apiResponse = ApiResponse.<UserInfoResponse>builder()
+                .result(userService.getMyInfo())
+                .message("Get user info successfully")
+                .build();
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PatchMapping("/update-info")
+    @Operation(summary = "Cập nhật thông tin cá nhân")
+    public ResponseEntity<?> updateMyInfo(@RequestBody @Valid UserInfoUpdateRequest userInfoUpdateRequest) {
+        userService.updateMyInfo(userInfoUpdateRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/change-password")
+    @Operation(summary = "Đổi mật khẩu")
+    public ResponseEntity<?> changePassword(@RequestBody @Valid UserChangePasswordRequest userChangePasswordRequest) {
+        userService.changePassword(userChangePasswordRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+}

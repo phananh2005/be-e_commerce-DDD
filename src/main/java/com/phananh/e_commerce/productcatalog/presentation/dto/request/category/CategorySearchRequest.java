5a342e704e5fe34a0c60c3334433681e9c77ee95
phananh2005 <phantran14122005@gmail.com>
2026-07-15 17:25:54 +0700

    refactor(product-catalog): remove unused search params from brand/category requests and simplify docs

--- src/main/java/com/phananh/e_commerce/productcatalog/presentation/dto/request/category/CategorySearchRequest.java
+++ src/main/java/com/phananh/e_commerce/productcatalog/presentation/dto/request/category/CategorySearchRequest.java
@@ -1,31 +1,12 @@
 package com.phananh.e_commerce.productcatalog.presentation.dto.request.category;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.phananh.e_commerce.core.util.StringUtils;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDateTime;
 
 @Data
 public class CategorySearchRequest {
 
     private String keyword;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime createdDateFrom;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime createdDateTo;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime modifiedDateFrom;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime modifiedDateTo;
 
     @Min(value = 0, message = "Page must be >= 0")
     private Integer page;
